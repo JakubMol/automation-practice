8d5ad8bb16925e439d09f904eeb6be1f2e62a11d
Jakub Mol <jakubmol@jakubs-mbp.lan>
2020-01-22 19:42:27 +0000

    feat: Added basic web driver helpers

---
+++ Source/Automation.Practice/Automation.Practice.WebDriver/WebDriverInteractionBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Automation.Practice.WebDriver
+{
+    public class WebDriverInteractionBase
+    {
+        protected IWebDriver _driver;
+        private TimeSpan _wait;
+        public string Url { get => _driver.Url; }
+
+        public WebDriverInteractionBase(int wait = 10)
+        {
+            /*
+             * Driver Creation should be extended
+             * to handle additional settings and browser bindings
+             */
+            _driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
+            _wait = TimeSpan.FromSeconds(wait);
+        }
+
+        public IWebElement GetVisableElement(By by)
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, _wait);
+            wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(by));
+            return wait.Until(ExpectedConditions.ElementIsVisible(by));
+        }
+
+        public IWebElement GetClickableElement(By by)
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, _wait);
+            wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(by));
+            return wait.Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+
+        public void Navigate(string url)
+        {
+            _driver.Url = url;
+            _driver.Navigate();
+        }
+    }
+}
