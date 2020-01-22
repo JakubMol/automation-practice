54f3cef45743f5b69b6b83f485e85f505a8e69f7
Jakub Mol <jakubmol@jakubs-mbp.lan>
2020-01-22 19:17:34 +0000

    feat: Specflow running on msTest

---
+++ Source/Automation.Practice/Automation.Practice/Test/SignIn/Feature/CanSignIn.feature
@@ -0,0 +1,10 @@
+﻿Feature: User Can sign in 
+	In order to sign in
+	As a user
+	I want to be able to access the system
+	
+@smoke
+Scenario: New User can sign in 
+	Given I am on the landing page
+	When I attempt to sign in 
+	Then I can access the system
---
+++ Source/Automation.Practice/Automation.Practice/Test/SignIn/Step/CanSignInStep.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Automation.Practice.Test.SignIn.Step
+{
+    [Binding]
+    public class CanSignInStep
+    {
+        [Given("I am on the landing page")]
+        public void LandingPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When("I attempt to sign in ")]
+        public void AttemptToSignIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then("I can access the system")]
+        public void CanAccessSystem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
