dca0b26bda417b574c5b79dfe12b39dc0bef3ace
Jakub Mol <jakubmol@Jakubs-MacBook-Pro.local>
2020-01-23 09:25:59 +0000

    feat: Added a User Details Test Data Builder

---
+++ Source/Automation.Practice/Automation.Practice.Data/UserDetails/UserDetailsBuilder.cs
@@ -0,0 +1,33 @@
+﻿using FizzWare.NBuilder;
+
+namespace Automation.Practice.Data.UserDetails
+{
+    public static class UserDetailsBuilder
+    {
+        public static UserDetails CreateNew()
+        {
+            return new Builder()
+                .CreateNew<UserDetails>()
+                .With(x => x.Address = CreateNewAddress())
+                .With(x => x.FirstName = Faker.NameFaker.FirstName())
+                .With(x => x.LastName = Faker.NameFaker.LastName())
+                .With(x => x.Password = Faker.NameFaker.FirstName())
+                .With(x => x.Email = $"{Faker.NumberFaker.Number()}{Faker.NameFaker.FirstName()}")
+                .With(x => x.PhoneNumber = Faker.PhoneFaker.Phone())
+                .Build();
+        }
+
+        public static UserAddress CreateNewAddress()
+        {
+            return new Builder()
+                .CreateNew<UserAddress>()
+                .With(x => x.AddressLine1 = Faker.LocationFaker.Street())
+                .With(x => x.AddressLine2 = Faker.LocationFaker.StreetName())
+                .With(x => x.City = Faker.LocationFaker.City())
+                .With(x => x.Country = "United States")
+                .With(x => x.County = Faker.LocationFaker.City())
+                .With(x => x.PostCode = Faker.LocationFaker.ZipCode())
+                .Build();
+        }
+    }
+}
