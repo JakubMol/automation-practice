7e68702a011314de07f66b7b3f85fd116bfcd773
Jakub Mol <jakubmol@Jakubs-MacBook-Pro.local>
2020-01-23 08:56:15 +0000

    feat: Added an actor implementation

---
+++ Source/Automation.Practice/Automation.Practice.Model/Actor.cs
@@ -0,0 +1,27 @@
+﻿using Automation.Practice.WebDriver;
+
+namespace Automation.Practice.Model
+{
+    public class Actor
+    {
+        private ActionHelpers _actionHelpers;
+        public string Url { get => _actionHelpers.Url; }
+
+        public Actor()
+        {
+            _actionHelpers = new ActionHelpers();
+        }
+
+        public Actor Can(IActorAction<ActionHelpers> ability)
+        {
+            ability.Execute(_actionHelpers);
+            return this;
+        }
+
+        public Actor CleanUp()
+        {
+            _actionHelpers.Quit();
+            return this;
+        }
+    }
+}
---
+++ Source/Automation.Practice/Automation.Practice.Model/IActorAction.cs
@@ -0,0 +1,7 @@
+﻿namespace Automation.Practice.Model
+{
+    public interface IActorAction<T>
+    {
+        void Execute(T ability);
+    }
+}
